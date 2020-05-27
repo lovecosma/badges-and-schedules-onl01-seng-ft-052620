1a5a524469534c9c52eab5114678110194877564
candescent-torrent-9701 <williamsmatthew333@gmail.com>
2020-05-27 19:47:56 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -28,5 +28,8 @@ def printer(attendees)
   @attendees = attendees
   @badges = batch_badge_creator(@attendees)
   @room_assignments = assign_rooms(@attendees)
-  puts @badges[0]
+  @counter = 0
+while @counter < @badges.length
+
+end
 end
